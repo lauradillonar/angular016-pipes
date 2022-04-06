5f4d191c620e390b92fe559fe48f1d8c128c6a22
lauradillonar <laura.dillon.ar@gmail.com>
2022-04-06 09:39:16 -0300

    Ejemplo de pipe personalizada

--- src/app/myFirstPipe.pipe.ts
+++ src/app/myFirstPipe.pipe.ts
@@ -5,7 +5,20 @@ import { Pipe, PipeTransform } from "@angular/core";
 })
 
 export class MyFirstPipe implements PipeTransform{
-    transform(valor: string) {
-        console.log('%c Pipe Done','background: #222; color: #bada55;');
+    transform(valor: string, args?:any) {
+        /* si el valor es nulo/undefined */
+        if (!valor){
+            return null;
+        }
+        let newValor: string;
+        if(valor.indexOf(' ') !== -1 && args === true){
+            newValor = valor.substring(0, valor.indexOf(' '));
+            console.log('%c Valor original: '+ valor,'background: #222; color: #bada55;');
+            console.log('%c Valor retornado: '+ newValor,'background: #bada55; color: #222;');
+            console.log('%c Argumentos input: '+ args,'background: red; color: #222;');
+            return newValor;
+        }else{
+            return valor;
+        }
     }
 }
